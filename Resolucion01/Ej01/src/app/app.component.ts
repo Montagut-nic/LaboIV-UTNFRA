ed118aa337d7e446cb5eeac5f73f4ef608ab57f0
Nicolas Montagut <nico.monta99@gmail.com>
2023-09-14 16:03:28 -0300

    ejercicio01 resuelto

--- Resolucion01/Ej01/src/app/app.component.ts
+++ Resolucion01/Ej01/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Ej01';
+  public btnCalcular: string = "Calcular";
+  public btnLimpiar: string = "Limpiar cuadros de textos";
+  public edadUno: any = "";
+  public edadDos: any = "";
+  public promedio: any = "";
+  public suma: any = "";
+
+  public Calcular(): void
+  {
+    this.Sumar();
+    this.Promediar();
+  }
+
+  public Sumar(): void
+  {
+    this.suma = parseInt(this.edadDos) + parseInt(this.edadUno);
+  }
+
+  public Promediar(): void
+  {
+    this.promedio = (parseInt(this.edadDos) + parseInt(this.edadUno)) / 2;
+  }
+
+  public Limpiar(): void
+  {
+    this.edadDos = "";
+    this.edadUno = "";
+    this.suma = "";
+    this.promedio = "";
+  }
+
 }
