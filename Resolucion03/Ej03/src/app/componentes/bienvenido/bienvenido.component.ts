2318cac9e2e46e38525bd11ac6dac98892395a66
Nicolas Montagut <nico.monta99@gmail.com>
2023-09-26 01:30:02 -0300

    correccion errores en ej03

--- Resolucion03/Ej03/src/app/componentes/bienvenido/bienvenido.component.ts
+++ Resolucion03/Ej03/src/app/componentes/bienvenido/bienvenido.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { getAuth} from '@firebase/auth';
-
+import { initializeApp } from "firebase/app";
 @Component({
   selector: 'app-bienvenido',
   templateUrl: './bienvenido.component.html',
@@ -12,4 +12,13 @@ export class BienvenidoComponent {
   
 }
 
-const auth = getAuth();
+const firebaseConfig = {
+  apiKey: "AIzaSyBx-M4xhshkazxUV_eFEX3TAaaYgR-tBnk",
+  authDomain: "ej03-montagut.firebaseapp.com",
+  projectId: "ej03-montagut",
+  storageBucket: "ej03-montagut.appspot.com",
+  messagingSenderId: "171451311356",
+  appId: "1:171451311356:web:cbc256b7056b7230076379"
+};
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
