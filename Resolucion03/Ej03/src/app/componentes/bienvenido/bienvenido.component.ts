2806ed797262428450098bb0db53d263f32d0ef1
Nicolas Montagut <nico.monta99@gmail.com>
2023-09-26 18:30:48 -0300

    ejercicio 03 listo

--- Resolucion03/Ej03/src/app/componentes/bienvenido/bienvenido.component.ts
+++ Resolucion03/Ej03/src/app/componentes/bienvenido/bienvenido.component.ts
@@ -1,24 +1,10 @@
 import { Component } from '@angular/core';
-import { getAuth} from '@firebase/auth';
-import { initializeApp } from "firebase/app";
+
 @Component({
   selector: 'app-bienvenido',
   templateUrl: './bienvenido.component.html',
   styleUrls: ['./bienvenido.component.scss']
 })
 export class BienvenidoComponent {
-
-  public usuario = auth.currentUser?.email;
   
 }
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBx-M4xhshkazxUV_eFEX3TAaaYgR-tBnk",
-  authDomain: "ej03-montagut.firebaseapp.com",
-  projectId: "ej03-montagut",
-  storageBucket: "ej03-montagut.appspot.com",
-  messagingSenderId: "171451311356",
-  appId: "1:171451311356:web:cbc256b7056b7230076379"
-};
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
