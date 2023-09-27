93daad95641f9e0d75e5cfb3e22c9bc3f0e65755
Nicolas Montagut <nico.monta99@gmail.com>
2023-09-27 13:24:07 -0300

    ej03 saludo bienvenido funciona

--- Resolucion03/Ej03/src/app/componentes/bienvenido/bienvenido.component.ts
+++ Resolucion03/Ej03/src/app/componentes/bienvenido/bienvenido.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
-
+import { getAuth} from 'firebase/auth';
+import { initializeApp } from "firebase/app";
 @Component({
   selector: 'app-bienvenido',
   templateUrl: './bienvenido.component.html',
   styleUrls: ['./bienvenido.component.scss']
 })
 export class BienvenidoComponent {
-  
+  public nombre : any = auth.currentUser?.email;
 }
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBx-M4xhshkazxUV_eFEX3TAaaYgR-tBnk",
+  authDomain: "ej03-montagut.firebaseapp.com",
+  projectId: "ej03-montagut",
+  storageBucket: "ej03-montagut.appspot.com",
+  messagingSenderId: "171451311356",
+  appId: "1:171451311356:web:cbc256b7056b7230076379"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
